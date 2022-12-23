392b2f80c3ab5b8915664b060491a2ff2426c7cd
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2022-12-23 11:27:02 +0900

    Changed it so that the Arch is no longer changable as it is not needed for our needs

--- update-repo.sh
+++ update-repo.sh
@@ -3,7 +3,7 @@
 APPDATA_FOLDER="appdata"
 REPO_FOLDER="$APPDATA_FOLDER/repo"
 
-rm -rf "$REPO_FOLDER/$ARCH/repodata"
+rm -rf "$REPO_FOLDER/x86_64/repodata"
 
 gpg --batch --quiet --import "$APPDATA_FOLDER/pgp-key.private"
 
@@ -12,12 +12,12 @@ if ! grep -q "%_gpg_name $GPG_KEY_NAME <$GPG_KEY_EMAIL>" ~/.rpmmacros; then
   echo "%_gpg_name $GPG_KEY_NAME <$GPG_KEY_EMAIL>" >> ~/.rpmmacros
 fi
 
-rpmsign --quiet --addsign "$REPO_FOLDER/$ARCH/"*.rpm |& \
+rpmsign --quiet --addsign "$REPO_FOLDER/x86_64/"*.rpm |& \
     grep -v "warning: Could not set GPG_TTY to stdin: Inappropriate ioctl for device" | \
-    grep -v "warning: $REPO_FOLDER/$ARCH/.*\.rpm already contains identical signature, skipping"
+    grep -v "warning: $REPO_FOLDER/x86_64/.*\.rpm already contains identical signature, skipping"
 
-createrepo -q "$REPO_FOLDER/$ARCH/"
+createrepo -q "$REPO_FOLDER/x86_64/"
 
-gpg --batch --quiet --detach-sign --armor "$REPO_FOLDER/$ARCH/repodata/repomd.xml"
+gpg --batch --quiet --detach-sign --armor "$REPO_FOLDER/x86_64/repodata/repomd.xml"
 
 chown -R abc:abc "$REPO_FOLDER"
--- update-zoom.sh
+++ update-zoom.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-SED_Parameter="s@^.*/prod/\([.0-9]*\)/zoom_$ARCH"
+SED_Parameter="s@^.*/prod/\([.0-9]*\)/zoom_x86_64"
 SED_Parameter+='.rpm.*$@\1@'
 
 APPDATA_FOLDER="appdata"
 REPO_FOLDER="$APPDATA_FOLDER/repo"
 
-mkdir -p "$REPO_FOLDER/$ARCH"
+mkdir -p "$REPO_FOLDER/x86_64"
 
-LATEST=$(curl -s -I HEAD "https://zoom.us/client/latest/zoom_$ARCH.rpm" | grep -E '^location:' | sed -e "$SED_Parameter")
+LATEST=$(curl -s -I HEAD "https://zoom.us/client/latest/zoom_x86_64.rpm" | grep -E '^location:' | sed -e "$SED_Parameter")
 
 echo "Got latest Zoom Version: $LATEST"
 
@@ -18,7 +18,7 @@ if [ "$LATEST" = "$(cat $APPDATA_FOLDER/version-zoom)" ]; then
     exit
 fi
 
-curl -s --fail -L -o "$REPO_FOLDER/$ARCH/zoom-$LATEST-1.$ARCH.rpm" "https://zoom.us/client/$LATEST/zoom_$ARCH.rpm"
+curl -s --fail -L -o "$REPO_FOLDER/x86_64/zoom-$LATEST-1.x86_64.rpm" "https://zoom.us/client/$LATEST/zoom_x86_64.rpm"
 
 echo "$LATEST" > $APPDATA_FOLDER/version-zoom
 echo "Updated saved Zoom Version"
