b866000ab523f8a77e29a93982affa6b94225385
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2022-12-26 09:20:24 +0900

    Maybe fixed the problem with execution with cron not working because of paths

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 cd /app || return
 
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
 runuser -u abc -- ./update-zoom.sh
 runuser -u abc -- ./update-dbeaver.sh
-./update-repo.sh
+runuser -u root -- ./update-repo.sh
